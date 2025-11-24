d0f5d527faeef53c0cfd261f0863f9392bb9c604
Andrés Padilla <andrespadillarobles@gmail.com>
2025-11-24 14:46:59 -0600

    Elminadas funciones muertas

--- extractor.h
+++
@@ -1,8 +0,0 @@
-#ifndef EXTRACTOR_H
-#define EXTRACTOR_H
-
-// Extrae la linea n del archivo archivo.txt
-// Retorna un puntero a la linea extraida (debe ser liberado por el caller)
-char *obtener_linea(int n);
-
-#endif
